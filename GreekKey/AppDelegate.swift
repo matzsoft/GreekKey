015b4301f8c9c33b4eb69718a42457b8db8467fa
Mark Johnson <markj@matzsoft.com>
2019-07-28 18:00:23 -0700

    Initial Commit

---
+++ GreekKey/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GreekKey
+//
+//  Created by Mark Johnson on 7/28/19.
+//  Copyright © 2019 matzsoft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
